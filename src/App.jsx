6dfd1767c414990e0e711aac2010256b873f96f9
tofercich <tofercich@Xubuntu.myguest.virtualbox.org>
2025-04-30 23:58:16 -0500

    add score and bestScore
    keep track of high score and current score

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
 import './App.css'
+import { useState } from 'react'
 
 function App() {  
+  const [bestScore, setBestScore] = useState(0);
+  const [currScore, setCurrScore] = useState(0);
+
+  if (currScore > bestScore) {
+    setBestScore(currScore);
+  }
+
   return (
-    <h1>Placeholder</h1>
+    <>
+      <h1>Placeholder</h1>
+      <button name='increase' onClick={() => setCurrScore(currScore + 1)}>Increase</button>
+      <button name='reset' onClick={() => setCurrScore(0)}>Reset</button>
+      <p>Score: {currScore}</p>
+      <p>Best Score: {bestScore}</p>
+    </>
   )
 }
 
